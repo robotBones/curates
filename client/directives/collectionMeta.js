dd2cbd41bf0df6540096975cb21c0927571c6451
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-09 11:37:10 -0700

    updated styles to have voting buttons, updated collection directives and template

--- client/directives/collectionMeta.js
+++ client/directives/collectionMeta.js
@@ -1,10 +1,17 @@
 angular.module('curates')
-.directive('collectionMeta', function() {
+
+.directive('collectionMeta', ['collectionFactory', function(collectionFactory) {
   return {
     restrict: 'E',
     scopes: {
       collection: '='
     },
-    templateUrl: 'directives/collectionMeta.html'
+    templateUrl: 'directives/collectionMeta.html',
+    link: function(scope) {
+      scope.castVote = function(collection, link, val) {
+        var user = 'Me';
+        collectionFactory.voteLink(collection, link, user, val);
+      };
+    }
   }
-});
+}]);
